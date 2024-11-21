1939ead9ca9f69e659b79edad76473965147c901
Steve Hoffing <shoffing@gmail.com>
2024-11-21 17:23:33 -0500

    allow showing characters with tokens

--- app/screens/info-tokens.tsx
+++ app/screens/info-tokens.tsx
@@ -1,9 +1,18 @@
+import Character from '@/app/components/character';
+import Token from '@/app/components/token';
+import { useAppSelector } from '@/app/hooks';
+import CharacterSelect from '@/app/screens/character-select';
+import { selectEdition } from '@/app/state/grim-slice';
 import { baseModalCloseButton, baseModalContainer, baseModalContent, baseModalScroll } from '@/app/styles/modals';
+import CharacterId from '@/constants/characters/character-id';
+import { getCharacterById } from '@/constants/characters/characters';
 import { ColorContainerType, Colors } from '@/constants/colors';
 
 import infoTokens from '@/data/info-tokens.json';
-import { useState } from 'react';
-import { ImageBackground, ScrollView, StyleSheet, TextStyle, View, ViewStyle } from 'react-native';
+import _ from 'lodash';
+import React, { useState } from 'react';
+import { ImageBackground, ScrollView, StyleSheet, TextStyle, useWindowDimensions, View, ViewStyle } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Button, FAB, Icon, IconButton, MD3Theme, Modal, Portal, Surface, Text, withTheme } from 'react-native-paper';
 
 interface InfoTokensProps {
@@ -13,6 +22,8 @@ interface InfoTokensProps {
 }
 
 function InfoTokens({ visible, onDismiss, theme }: InfoTokensProps) {
+  const edition = useAppSelector(state => selectEdition(state.grim));
+
   const styles = StyleSheet.create({
     modalContainer: baseModalContainer(theme),
     modalContent: {
@@ -42,31 +53,33 @@ function InfoTokens({ visible, onDismiss, theme }: InfoTokensProps) {
       position: 'absolute',
       bottom: 20,
       right: 20,
+      borderWidth: 2,
+      borderColor: theme.colors.outline,
     },
   });
 
+  const [showingInfoVisible, setShowingInfoVisible] = useState(false);
+  const showInfo = () => setShowingInfoVisible(true);
+  const hideInfo = () => {
+    clearShowingCharacters();
+    setShowingInfoVisible(false);
+  };
   const [showingInfo, setShowingInfo] = useState<typeof infoTokens[number]>(infoTokens[2]);
-  const hideShowingInfo = () => setShowingInfo(undefined);
 
-  const infoTokenCards = infoTokens.map(info => {
-    const background = Colors.colorContainer(theme.dark)[info.color as ColorContainerType];
-    const foreground = Colors.onColorContainer(theme.dark)[info.color as ColorContainerType];
-    return (
-      <Button
-        mode="contained"
-        labelStyle={theme.fonts.bodyLarge}
-        contentStyle={styles.infoButton}
-        buttonColor={background}
-        textColor={foreground}
-        rippleColor={foreground}
-        icon={() => (
-          <Icon source={info.icon} size={32} color={foreground}/>
-        )}
-        onPress={() => setShowingInfo(info)}>
-        {info.text}
-      </Button>
-    );
-  });
+  const [characterSelectVisible, setCharacterSelectVisible] = useState(false);
+  const showCharacterSelect = () => setCharacterSelectVisible(true);
+  const hideCharacterSelect = () => setCharacterSelectVisible(false);
+
+  const [showingCharacters, setShowingCharacters] = useState<CharacterId[]>([]);
+  const addShowingCharacter = (character: CharacterId) => setShowingCharacters([...showingCharacters, character]);
+  const removeShowingCharacter = (idx: number) => setShowingCharacters([
+    ..._.take(showingCharacters, idx),
+    ..._.drop(showingCharacters, idx + 1),
+  ]);
+  const clearShowingCharacters = () => setShowingCharacters([]);
+
+  const containerImage = showingCharacters.length > 0 ? require('@/assets/images/info-token/info-token-container-tall.webp') : require('@/assets/images/info-token/info-token-container.webp');
+  const containerBackgroundImage = showingCharacters.length > 0 ? require('@/assets/images/info-token/info-token-container-tall-bg.webp') : require('@/assets/images/info-token/info-token-container-bg.webp');
 
   const showingInfoBackground = Colors.colorContainer(theme.dark)[showingInfo?.color as ColorContainerType];
   const showingInfoForeground = Colors.onColorContainer(theme.dark)[showingInfo?.color as ColorContainerType];
@@ -84,7 +97,54 @@ function InfoTokens({ visible, onDismiss, theme }: InfoTokensProps) {
     textAlign: 'center',
     marginHorizontal: 64,
     maxHeight: 350,
+    fontFamily: 'NewRocker-Regular',
   } : null);
+
+  const infoTokenButtons = infoTokens.map((info, idx) => {
+    const background = Colors.colorContainer(theme.dark)[info.color as ColorContainerType];
+    const foreground = Colors.onColorContainer(theme.dark)[info.color as ColorContainerType];
+    return (
+      <Button
+        key={`info-token-button-${idx}`}
+        mode="contained"
+        labelStyle={theme.fonts.bodyLarge}
+        contentStyle={styles.infoButton}
+        buttonColor={background}
+        textColor={foreground}
+        rippleColor={foreground}
+        icon={() => (
+          <Icon source={info.icon} size={32} color={foreground}/>
+        )}
+        onPress={() => {
+          setShowingInfo(info);
+          showInfo();
+        }}>
+        {info.text}
+      </Button>
+    );
+  });
+
+  // Turns out packing squares into a rectangle is NP-hard. Who knew?
+  // A friend who is smarter than me says "I believe that reduces to the knapsack problem".
+  // Oh well, here is an approximation that works pretty good with numbers up to ~10.
+  const { width, height } = useWindowDimensions();
+  const tokenSize = 0.44 * Math.sqrt(width * height / showingCharacters.length);
+  const showingCharacterTokens = showingCharacters.map((characterId, idx) => {
+    const characterData = getCharacterById(characterId);
+    return (
+      <View key={`show-character-token-${idx}`} style={{ width: tokenSize, height: tokenSize }}>
+        <Token
+          fixed
+          size={tokenSize}
+          tokenStyle={{ aspectRatio: 1 }}
+          centerContent={
+            <Character character={characterData} team={characterData.team}/>
+          }
+          bottomText={characterData.name}
+          onPress={() => removeShowingCharacter(idx)}/>
+      </View>
+    );
+  });
 
   return (
     <Portal>
@@ -96,7 +156,7 @@ function InfoTokens({ visible, onDismiss, theme }: InfoTokensProps) {
 
         <ScrollView>
           <Surface style={styles.modalScroll}>
-            {infoTokenCards}
+            {infoTokenButtons}
           </Surface>
         </ScrollView>
 
@@ -111,61 +171,67 @@ function InfoTokens({ visible, onDismiss, theme }: InfoTokensProps) {
       </Modal>
 
       <Modal
-        visible={!!showingInfo}
+        visible={showingInfoVisible}
         dismissable={false}
         style={showModalStyle}
         contentContainerStyle={showModalContentStyle}>
         <ImageBackground
           source={require('@/assets/images/info-token/info-token-background.png')}
-          style={{
-            alignItems: 'center',
-            height: '100%',
-            width: '100%',
-          }}
-          imageStyle={{
-            opacity: 0.33,
-            resizeMode: 'repeat',
-          }}
+          style={{ alignItems: 'center', height: '100%', width: '100%' }}
+          imageStyle={{ opacity: 0.33, resizeMode: 'repeat' }}
           fadeDuration={0}>
           <ImageBackground
-            source={require('@/assets/images/info-token/info-token-container-bg.webp')}
-            style={{
-              alignItems: 'center',
-              height: '100%',
-              width: '95%',
-            }}
-            imageStyle={{
-              resizeMode: 'contain',
-              width: '100%',
-            }}
+            source={containerBackgroundImage}
+            style={{ alignItems: 'center', height: '100%', width: '95%' }}
+            imageStyle={{ resizeMode: 'contain', width: '100%' }}
             tintColor={showingInfoBackground}>
             <ImageBackground
-              source={require('@/assets/images/info-token/info-token-container.webp')}
-              style={{
-                alignItems: 'center',
-                height: '100%',
-                justifyContent: 'center',
-                width: '100%',
-              }}
-              imageStyle={{
-                resizeMode: 'contain',
-                width: '100%',
-              }}>
+              source={containerImage}
+              style={{ alignItems: 'center', height: '100%', justifyContent: 'center', width: '100%' }}
+              imageStyle={{ resizeMode: 'contain', width: '100%' }}>
               <View style={{
                 alignItems: 'center',
                 padding: 64,
                 position: 'relative',
                 top: showingInfo?.icon ? -32 : 0,
+                gap: 12,
               }}>
                 <Icon source={showingInfo?.icon} size={128}/>
                 <Text variant="displayLarge" style={showModalTextStyle} adjustsFontSizeToFit>{showingInfo?.text}</Text>
+                <GestureHandlerRootView style={{ flex: 0, alignItems: 'center', gap: 12 }}>
+                  <View style={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    gap: 12,
+                    justifyContent: 'center',
+                  }}>
+                    {showingCharacterTokens}
+                  </View>
+                  {
+                    showingInfo?.showCharacters ?
+                      <IconButton icon="plus" mode="contained" size={64} onPress={showCharacterSelect}/> : null
+                  }
+                </GestureHandlerRootView>
               </View>
             </ImageBackground>
           </ImageBackground>
         </ImageBackground>
-        <FAB mode="elevated" icon="close" onPress={hideShowingInfo} size="medium"
-             style={styles.showInfoCloseButton}/>
+        <FAB
+          mode="elevated"
+          icon="close"
+          onPress={hideInfo}
+          size="large"
+          style={styles.showInfoCloseButton}/>
       </Modal>
+
+      <CharacterSelect
+        visible={characterSelectVisible}
+        edition={edition}
+        onSelect={character => {
+          addShowingCharacter(character);
+          hideCharacterSelect();
+        }}
+        onDismiss={hideCharacterSelect}/>
     </Portal>
   );
 }
